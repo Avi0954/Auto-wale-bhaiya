7ba5b503f2efd3a42bb32b87f9f8321c6039e340
Avinash <bkp2544k@gmail.com>
2026-08-12 00:06:40 +0530

    chore: update playlist data

--- src/data/autoDriver90sPlaylist.ts
+++ src/data/autoDriver90sPlaylist.ts
@@ -2,66 +2,115 @@ import type { Song } from '../types';
 
 export const autoDriver90sPlaylist: Song[] = [
   {
-    id: "song-1786454389655-0",
+    id: "song-1786473215601-0",
     title: "Tujhe Pyar Se Dekhne Wala Ek Dil Hai | Ek Rishtaa | Kumar Sanu | Alka Yagnik | Akshay, Karisma",
     artist: "Bollywood Classic Hits",
     artwork: "https://i.ytimg.com/vi/Vi9HvK2h9Rw/hqdefault.jpg",
     youtubeVideoId: "Vi9HvK2h9Rw"
   },
   {
-    id: "song-1786454389655-1",
+    id: "song-1786473215601-1",
     title: "Mubarak Ho Tumko Ye Shaadi Tumhari - Haan Maine Bhi Pyaar Kiya | Udit N | Akshay, Karisma, Abhishek",
     artist: "90's Gaane",
     artwork: "https://i.ytimg.com/vi/QBEqQG5kYC0/hqdefault.jpg",
     youtubeVideoId: "QBEqQG5kYC0"
   },
   {
-    id: "song-1786454389655-2",
+    id: "song-1786473215601-2",
     title: "Hum Pyaar Hai Tumhare - Lyrical | Haan Maine Bhi Pyaar Kiya | Kumar Sanu, Alka Yagnik | Love Songs",
     artist: "Tips Official",
     artwork: "https://i.ytimg.com/vi/0OLJaYETWoA/hqdefault.jpg",
     youtubeVideoId: "0OLJaYETWoA"
   },
   {
-    id: "song-1786454389655-3",
+    id: "song-1786473215601-3",
     title: "Jhuki Jhuki Nazar Teri Kamaal - Lyrical | Raja | Madhuri Dixit | Sanjay | Alka Yagnik | Udit Narayan",
     artist: "Tips Official",
     artwork: "https://i.ytimg.com/vi/vf0au_2CYYE/hqdefault.jpg",
     youtubeVideoId: "vf0au_2CYYE"
   },
   {
-    id: "song-1786454389655-4",
+    id: "song-1786473215601-4",
     title: "Kisi Se Tum Pyar Karo | Akshay Kumar | Lara Dutta | Andaaz (2003) | Kumar Sanu | Dard Bhare Gaane",
     artist: "Manju K Gaane",
     artwork: "https://i.ytimg.com/vi/HBJbDyFLH6Q/hqdefault.jpg",
     youtubeVideoId: "HBJbDyFLH6Q"
   },
   {
-    id: "song-1786454389655-5",
+    id: "song-1786473215601-5",
     title: "Aaye Ho Meri Zindagi  mei tum bahar banke   Female Version   Lyrics1",
     artist: "TSA Media",
     artwork: "https://i.ytimg.com/vi/DQFiNnSWs1g/hqdefault.jpg",
     youtubeVideoId: "DQFiNnSWs1g"
   },
   {
-    id: "song-1786454389655-6",
-    title: "Aashiqui  Main Duniya Bhula Doonga | High Quality Audio",
-    artist: "The Mastering Project",
-    artwork: "https://i.ytimg.com/vi/_X5zoVwZQaY/hqdefault.jpg",
-    youtubeVideoId: "_X5zoVwZQaY"
-  },
-  {
-    id: "song-1786454389655-7",
+    id: "song-1786473215601-6",
     title: "Aisi Deewangi - Lyrical Video | Deewana | Shahrukh Khan | Divya Bharti | Ishtar Music",
     artist: "Ishtar Music",
     artwork: "https://i.ytimg.com/vi/_YjSmLlmqLM/hqdefault.jpg",
     youtubeVideoId: "_YjSmLlmqLM"
   },
   {
-    id: "song-1786454389655-8",
+    id: "song-1786473215601-7",
     title: "Main Agar Saamne | Raaz | Dino Morea | Bipasha Basu | Abhijeet & Alka Yagnik | Hindi Hit Songs",
     artist: "Tips Official",
     artwork: "https://i.ytimg.com/vi/B2aHXbnu9dI/hqdefault.jpg",
     youtubeVideoId: "B2aHXbnu9dI"
+  },
+  {
+    id: "song-1786473215601-8",
+    title: "Ghoonghat Ki Aad Se | Hum Hain Rahi Pyar Ke | Kumar Sanu | Alka Yagnik | 1993",
+    artist: "90's Gaane",
+    artwork: "https://i.ytimg.com/vi/xnhjxG79Ttc/hqdefault.jpg",
+    youtubeVideoId: "xnhjxG79Ttc"
+  },
+  {
+    id: "song-1786473215601-9",
+    title: "Tumse Milne Ko Dil Karta Hai (Lyrical) Ajay Devgn | Madhoo | Alka Y | Kumar S | Phool Aur Kaante",
+    artist: "Best Of Kumar Sanu",
+    artwork: "https://i.ytimg.com/vi/tJCzv0hQDmM/hqdefault.jpg",
+    youtubeVideoId: "tJCzv0hQDmM"
+  },
+  {
+    id: "song-1786473215601-10",
+    title: "Saaton Janam Tujhko Paate | Gori Tere Nainon Mein Hum Bas | Govinda, Karisma | Kumar Sanu Hindi Song",
+    artist: "90's Gaane",
+    artwork: "https://i.ytimg.com/vi/55tpRoSjCes/hqdefault.jpg",
+    youtubeVideoId: "55tpRoSjCes"
+  },
+  {
+    id: "song-1786473215601-11",
+    title: "Raah Mein Unse Mulaqat Ho Gayi | Ajay Devgn, Tabu | Kumar Sanu, Alka Yagnik | Vijaypath (1994)",
+    artist: "Hot Hits Hindi",
+    artwork: "https://i.ytimg.com/vi/AQSRq6eGWp0/hqdefault.jpg",
+    youtubeVideoId: "AQSRq6eGWp0"
+  },
+  {
+    id: "song-1786473215601-12",
+    title: "Main Duniya Bhula Dunga | 4K Video Song | Aashiqui | Rahul Roy, Anu Agrawal | Kumar Sanu",
+    artist: "UnknowN",
+    artwork: "https://i.ytimg.com/vi/_Y3oWdjgI6U/hqdefault.jpg",
+    youtubeVideoId: "_Y3oWdjgI6U"
+  },
+  {
+    id: "song-1786473215601-13",
+    title: "Tu Pyar Hai Kisi Aur Ka - Video Song | Kumar Sanu, Anuradha Paudwal | Aamir Khan, Pooja Bhatt",
+    artist: "T-Series Bollywood Classics",
+    artwork: "https://i.ytimg.com/vi/47DstHmE-bE/hqdefault.jpg",
+    youtubeVideoId: "47DstHmE-bE"
+  },
+  {
+    id: "song-1786473215601-14",
+    title: "Dulhe Ka Sehra - HD VIDEO SONG | Akshay Kumar & Shilpa Shetty |Dhadkan |90's Bollywood Marriage Song",
+    artist: "Md Abu Syed Tanu",
+    artwork: "https://i.ytimg.com/vi/JHZksjgLMOk/hqdefault.jpg",
+    youtubeVideoId: "JHZksjgLMOk"
+  },
+  {
+    id: "song-1786473215601-15",
+    title: "Aapke Pyaar Mein Hum | Alka Yagnik | Razz | MP3 Song |",
+    artist: "90s Dhamaka Song",
+    artwork: "https://i.ytimg.com/vi/Mbj3hTK-Fy4/hqdefault.jpg",
+    youtubeVideoId: "Mbj3hTK-Fy4"
   }
 ];
